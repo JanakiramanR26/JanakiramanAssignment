9671ab3a16e8d56e98189afdef4e47ce1845d78f
janaki <janaki26092001@gmail.com>
2023-08-31 11:14:09 +0530

    loop statement

---
+++ loop-statement(31-08-2023)/loop.js
@@ -0,0 +1,8 @@
+for(num1=1; num1<=20;num1++){
+    console.log(`<<<-------(${num1}Tables)-------->>>`);
+    for(num2=1; num2<=20;num2++){
+        console.log(`${num2} * ${num1} = ${num1*num2}`);
+        
+    }
+   
+}
